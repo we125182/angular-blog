505201b4c17d8f560f6460434e2bb9bdce22ce38
we125182 <we1215107007@163.com>
2019-08-26 21:30:11 +0800

    feat(hmr): 新增开发环境下的热更新

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,33 @@
-import { enableProdMode } from '@angular/core';
+import { ApplicationRef, enableProdMode, NgModuleRef } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { createNewHosts } from '@angularclass/hmr';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+const bootstrap = () => platformBrowserDynamic().bootstrapModule(AppModule);
+
+if (environment.hmr) {
+  // tslint:disable
+  if (module['hot']) {
+    let ngModule: NgModuleRef<any>;
+    module['hot'].accept();
+    bootstrap().then(mod => ngModule = mod);
+    module['hot'].dispose(() => {
+      const appRef: ApplicationRef = ngModule.injector.get(ApplicationRef);
+      const elements = appRef.components.map(c => c.location.nativeElement);
+      const makeVisible = createNewHosts(elements);
+      ngModule.destroy();
+      makeVisible();
+    });
+  } else {
+    console.error('HMR is not enabled for webpack-dev-server!');
+    console.log('Are you using the --hmr flag for ng serve?');
+  }
+} else {
+  bootstrap().catch(err => console.log(err));
+}
